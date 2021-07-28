9ef252d5a1e91479f82f3333f1e58d22b80b0db5
Jose matias <jose@pop-os.localdomain>
2021-07-28 00:43:22 -0400

    settings

--- pasteleria/settings.py
+++ pasteleria/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-*di&7q-fezfxvp_%tinqv%q9%$id82#=t8+ymmk6$+4kr$ol5m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://apiview-pasteleria.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
